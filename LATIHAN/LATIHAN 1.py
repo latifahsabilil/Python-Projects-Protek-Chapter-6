5feeb1dbdaf37beee7e57898a8f865788f5bf637
latifahsabilil <71626855+latifahsabilil@users.noreply.github.com>
2020-11-01 23:43:43 +0700

    Create LATIHAN 1.py

---
+++ LATIHAN/LATIHAN 1.py
@@ -0,0 +1,38 @@
+#1
+def isPythagoras(a, b, c):
+    x = a**2 + b**2 == c**2
+    return x
+a = 3
+b = 4
+c = 5
+isPythagoras(a, b, c)
+print(isPythagoras(a, b, c))
+
+#2
+def isPythagoras(a, b, c):
+    x = a**2 + b**2 == c**2
+    return x
+a = 5
+b = 9
+c = 12
+isPythagoras(a, b, c)
+print(isPythagoras(a, b, c))
+
+#3
+def isPythagoras(a, b, c):
+    x = a**2 + b**2 == c**2
+    return x
+a = 8
+b = 6
+c = 10
+isPythagoras(a, b, c)
+print(isPythagoras(a, b, c))
+
+def isPythagoras(a, b, c):
+    x = a**2 + b**2 == c**2
+    return x
+a = 7
+b = 8
+c = 11
+isPythagoras(a, b, c)
+print(isPythagoras(a, b, c))
